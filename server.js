18c50246d82562f7e07448a4076c5d80f61ec50a
ChekoButton <dszczotka15@gmail.com>
2023-01-19 09:47:32 +0100

    Working clone on localhost

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+app.set('view engine', "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true }))
+
+
+
+
+
+const Document = require('./models/Document')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+mongoose.connect("mongodb://localhost/copybin",{
+    // useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    
+})
+
+//   if (mongoose.connection.readyState === 1) {
+//     console.log("Mongoose is connected to the database");
+//   } else {
+//     console.log("Mongoose is not connected to the database");
+//   }
+  
+ 
+app.get("/", (req, res) =>{
+
+    const code = `Welcome to CopyBin!
+
+This project is clone of the known app HasteBin.
+    
+Use the commands in the top right conrner
+to create a new file to share with others.
+
+Type what you want me to see, click "Save", and then copy the URL.
+Send that URL to someone and they'll see what you see.`
+    res.render("view-display.ejs", { code, language: 'plaintext' })
+})
+
+app.get ("/new", (req, res) =>{
+    res.render("new.ejs")
+})
+
+app.post('/save', async (req,res) =>{
+    const value = req.body.value
+    try  {
+        const document = await Document.create({ value })
+        res.redirect(`/${document.id}`)
+    } catch (e) {
+        res.render("new", { value })
+    }
+    console.log(value);
+})
+
+
+app.get('/:id/duplicate', async (req, res) => {
+    const id = req.params.id
+    try {
+        const document = await Document.findById(id)
+
+        res.render('new', { value: document.value })
+    } catch (e){
+        res.redirect(`/${id}`)
+    }
+})
+
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const document = await Document.findById(id)
+
+        res.render('view-display', {code: document.value, id })
+    } catch (e){
+        res.redirect('/')
+    }
+})
+
+app.listen(3000)
